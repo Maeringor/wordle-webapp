3466edc44c43a3ea7fc51189a6efa3e88be4b7f5
Maeringor <business.maurice@conrad.email>
2022-06-09 16:04:06 +0200

    IS:M cumstom-word.php fertiggestellt + info button erstellt

--- custom-word.php
+++ custom-word.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Page Title</title>
+    <title>Worlde</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <!-- Globeles Stylesheet -->  
     <link rel='stylesheet' type='text/css' media='screen' href='/css/design.css'>
     <link rel='stylesheet' type='text/css' media='screen' href='/css/nav.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='/css/elements.css'>
 
     <link rel='stylesheet' type='text/css' media='screen' href='/css/custom-word.css'>
 </head>
@@ -20,14 +21,13 @@
 
         <!-- headline area -->
         <div class="headline-container primary-textcol master-head letter-spacing-small medium">
-            <div class="headline">Send us a custom word to be</div>
+            <div class="headline"><div class="quadrat-text-element light-blue-bg quadrat-pos"></div>Send us a custom word to be</div>
             <div class="headline-subcontainer">
                  <div class="headline">part of the wordle community
                       <object data="/rescources/svg/small-dot.svg" width="15px" height="15px"></object>
                  </div> 
             </div>
             
-            <div class="quadrat-text-element"></div>
          </div>
          <!-- headline area end -->
 
@@ -35,18 +35,30 @@
             <div class="content-container">
 
                 <div class="input-container">
-                    <div class="input-head sub-head letter-spacing-small bold secondary-textcol">Your own word in wordle</div>
-
-                    <div class="form" action="">
-                        <div class="input-field">
-                            <input minlength="5" maxlength="5" autocomplete="off" class="third-head light-blue-bg" type="text" id="custom" placeholder=" ">
-                            <label for="custom" class="form-lable paragraph medium light-blue-bg">input word</label>
-                            <!-- info button -->
-
-                            <!-- cta button -->
-                            <button class="main-button">Submit</button>
-                        </div>
+                    <div class="inner-container">
                         
+                        <div class="input-head sub-head letter-spacing-small bold secondary-textcol">Your own word in wordle</div>
+
+                        <form class="form" action="">
+                            <div class="input-field">
+                                <input minlength="5" maxlength="5" autocomplete="off" class="third-head light-blue-bg" type="text" id="custom" placeholder=" ">
+                                <label for="custom" class="form-lable paragraph medium light-blue-bg">input word</label>
+                                <!-- info button -->
+                                <div class="info-btn-position paragraph">
+                                    <?php require 'html_structures/infobutton.php';
+
+                                        setInfoButton("Please enter a word you think could benefit the wordle community. 
+                                        Other palyers will be notified when they played your word :)");
+                                    ?>
+                                </div>
+                            </div>
+                            
+                            <!-- cta button -->
+                            <div class="cta-btn">
+                                    <button type="button" class="main-button blue-bg paragraph letter-spacing-big secondary-textcol">Submit</button>
+                            </div>
+                        </form>
+
                     </div>
                 </div>
 
---
+++ html_structures/infobutton.php
@@ -0,0 +1,6 @@
+<?php
+    // prints out a info button with var text (required styles: elements.css)
+    function setInfoButton($text){
+        echo "<div class='info-button-container'><div class='info-btn'>i</div><div class='content'>$text</div></div>";
+    }
+?>
